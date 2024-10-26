c22d5af497119feeb15cb06377531b74eb72ff3a
qiaohao <qiaohao.peng@outlook.com>
2024-10-27 04:10:04 +0800

    add: radicale

---
+++ radicale/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+username=${RC_AUTH_USERNAME:-radicale}
+password=${RC_AUTH_PASSWORD:-radicale}
+web_type=${RC_WEB_TYPE:-internal}
+
+cat >> /etc/radicale/users <<EOF
+${username}:${password}
+EOF
+
+cat >> /etc/radicale/config <<EOF
+[server]
+hosts = 0.0.0.0:5232
+
+[auth]
+type                = htpasswd
+htpasswd_filename   = /etc/radicale/users
+htpasswd_encryption = plain
+
+[storage]
+filesystem_folder = /var/lib/radicale/collections
+
+[web]
+type = ${web_type}
+EOF
+
+radicale
+
